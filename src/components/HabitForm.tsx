45588ce1d8341c73ca1b797f3a10d91b582fe049
Admir Bjelic <admir.bjelic@outlook.com>
2026-09-24 21:33:43 +0200

    Add Habit Item Component

--- src/components/HabitForm.tsx
+++ src/components/HabitForm.tsx
@@ -7,7 +7,7 @@ export function HabitForm() {
             py-2 outline-none focus-visible:ring-2 
             focus-visible:ring-blue-500"
             placeholder="New habit..."/>
-            <Button>Add Habit</Button>
+            <Button className="rounded-lg px-4 py-2 font-medium">Add Habit</Button>
         </form>
     )
 }
